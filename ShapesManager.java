837b438268c9b0339a9a9d52c13d89a2d7998bbf
MogaDenis <moga_denis@yahoo.com>
2023-10-10 18:48:26 +0300

    Finished work

--- ShapesManager.java
+++ ShapesManager.java
@@ -5,7 +5,7 @@ import view.ConsoleView;
 // 7. Se da o colectie de mai multe obiecte
 // avand forme de cuburi, sfere si cilindrii.
 // Sa se afiseze obiectele avand volumul mai mare
-// decit 25cm3.
+// decat 25cm3.
 
 public class ShapesManager 
 {
